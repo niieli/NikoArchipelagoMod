bf2a6e9ffac59f83fb54c0cd7b0902332a14e2d1
niieli <96092663+niieli@users.noreply.github.com>
2024-01-26 19:46:58 +0100

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Archipelago.MultiClient.Net;
+using BepInEx;
+using HarmonyLib;
+using HarmonyLib.Tools;
+using KinematicCharacterController.Core;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Serialization;
+using UnityEngine.UIElements;
+
+namespace NikoArchipelago
+{
+
+
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    [BepInProcess("Here Comes Niko!.exe")]
+    public class Plugin : BaseUnityPlugin
+    {
+        public Notification test;
+        public static CustomButton AptestButton;
+        private List<string> _saveDataCoinFlag, _saveDataCassetteFlag, _saveDataFishFlag, _saveDataMiscFlag, _saveDataLetterFlag, _saveDataGeneralFlag;
+        private List<bool> _unlockedLevels;
+        private int _nFlg1, _nFlg2, _nFlg3, _nFlg4, _nFlg5, _nFlg6, _coinTotal, _coinOld, _levelIndex, _currentLevel, _currentTemp;
+        private Notification _newFlag = new ();
+        private Notification _note = ScriptableObject.CreateInstance<Notification>();
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+
+
+        public void Load()
+        {
+            LoadPatches();
+        }
+
+        public void Start()
+        {
+            //
+        }
+
+        public void Update()
+        {
+            //scrGameSaveManager.instance.SaveGame();
+            if (!string.Equals(scrGameSaveManager.saveName, "NieDebug2024Save", StringComparison.Ordinal))
+            {
+                // scrGameSaveManager.saveName = "APLogicSave";
+                scrGameSaveManager.saveName = "NieDebug2024Save";
+            }
+            Flags();
+        }
+        
+        
+        
+        public void Flags()
+        {
+            // Assign lists
+            _saveDataCoinFlag = scrWorldSaveDataContainer.instance.coinFlags;
+            _saveDataCassetteFlag = scrWorldSaveDataContainer.instance.cassetteFlags;
+            _saveDataFishFlag = scrWorldSaveDataContainer.instance.fishFlags;
+            _saveDataLetterFlag = scrWorldSaveDataContainer.instance.letterFlags;
+            _saveDataMiscFlag = scrWorldSaveDataContainer.instance.miscFlags;
+            _saveDataGeneralFlag = scrGameSaveManager.instance.gameData.generalGameData.generalFlags;
+            _unlockedLevels = scrGameSaveManager.instance.gameData.generalGameData.unlockedLevels;
+            _coinTotal = scrGameSaveManager.instance.gameData.generalGameData.coinAmountTotal;
+            _currentLevel = scrWorldSaveDataContainer.instance.worldIndex;
+            if (_saveDataCoinFlag.Count > _nFlg1) // Check if a new flag has been added
+            {
+                Logger.LogFatal("New Coin Flag! '" + _saveDataCoinFlag[_nFlg1] + "'");
+                _nFlg1++;
+            }
+            else if (_nFlg1 > _saveDataCoinFlag.Count) // Probably not needed, but just in case of a level transition
+            {
+                _nFlg1 = 0;
+            }
+
+            if (_saveDataCassetteFlag.Count > _nFlg2) // Check if a new flag has been added
+            {
+                Logger.LogFatal("New Cassette Flag! '" + _saveDataCassetteFlag[_nFlg2] + "'");
+                _nFlg2++;
+            }
+            else if (_nFlg2 > _saveDataCassetteFlag.Count) // Probably not needed, but just in case of a level transition
+            {
+                _nFlg2 = 0;
+            }
+
+            if (_saveDataFishFlag.Count > _nFlg3) // Check if a new flag has been added
+            {
+                Logger.LogFatal("New Fish Flag! '" + _saveDataFishFlag[_nFlg3] + "'");
+                _nFlg3++;
+            }
+            else if (_nFlg3 > _saveDataFishFlag.Count) // Probably not needed, but just in case of a level transition
+            {
+                _nFlg3 = 0;
+            }
+
+            if (_saveDataMiscFlag.Count > _nFlg4) // Check if a new flag has been added
+            {
+                Logger.LogFatal("New Misc Flag! '" + _saveDataMiscFlag[_nFlg4] + "'");
+                _nFlg4++;
+            }
+            else if (_nFlg4 > _saveDataMiscFlag.Count) // Probably not needed, but just in case of a level transition
+            {
+                _nFlg4 = 0;
+            }
+
+            if (_saveDataLetterFlag.Count > _nFlg5) // Check if a new flag has been added
+            {
+                Logger.LogFatal("New Letter Flag! '" + _saveDataLetterFlag[_nFlg5] + "'");
+                _nFlg5++;
+            }
+            else if (_nFlg5 > _saveDataLetterFlag.Count) // Probably not needed, but just in case of a level transition
+            {
+                _nFlg5 = 0;
+            }
+            
+            if (_saveDataGeneralFlag.Count > _nFlg6) // Check if a new flag has been added
+            {
+                Logger.LogFatal("New General Flag! '" + _saveDataGeneralFlag[_nFlg6] + "'");
+                _nFlg6++;
+            }
+            else if (_nFlg6 > _saveDataGeneralFlag.Count) // Probably not needed, but just in case of a level transition
+            {
+                _nFlg6 = 0;
+            }
+
+            if (_unlockedLevels.Count > _levelIndex)
+            {
+                Logger.LogWarning("New Level unlocked! " + _unlockedLevels[_levelIndex]);
+                _levelIndex++;
+            }
+
+            if (_coinTotal > _coinOld)
+            {
+                Logger.LogWarning("Total Coin Count = " + _coinTotal);
+                _coinOld++;
+            }
+
+            if (_currentTemp == _currentLevel)
+            {
+                Logger.LogInfo("Current Level Index: '" + _currentLevel + "'");
+                _currentTemp = _currentLevel;
+            }
+        }
+
+        public void StuffX()
+        {
+            scrTrainManager.instance.UseTrain(1,false);
+        }
+
+        public void NewItem()
+        {
+            _newFlag.key = "pls work man i have kids and wife";
+            _newFlag.name = "Help";
+            _newFlag.duration = 5f;
+            scrNotificationDisplayer.instance.AddNotification(_newFlag);
+            _note.key = "This is a new note by nieli";
+            scrNotificationDisplayer.instance.AddNotification(_note);
+        }
+
+        public void SoundFix()
+        {
+            GameOptions.AudioOptions.ForEach(Logger.LogMessage);
+        }
+        
+        // Apply Harmony patches (?)
+        public static void LoadPatches()
+        {
+            HarmonyFileLog.Enabled = true;
+            var harmony = new Harmony("apniko");
+            harmony.PatchAll();
+        }
+        
+        public Rect windowRect = new Rect(600, 500, 400, 200);
+
+        void WindoFunc(int windowID)
+        {
+            GUI.color = Color.yellow;
+            if (GUI.Button(new Rect(10, 20, 100, 20), "Current Flags"))
+            {
+                // Notification note = ScriptableObject.CreateInstance<Notification>();
+                // note.key = "test";
+                // scrNotificationDisplayer.instance.AddNotification(note);
+                //scrWorldSaveDataContainer.instance.coinFlags.Add("volley");
+                //Logger.LogInfo(scrGameSaveManager.instance.gameData.worldsData.ToList());
+                //System.Collections.Generic.List<scrWorldSaveDataContainer>;
+                _saveDataCoinFlag.ForEach(Logger.LogFatal);
+                _saveDataCassetteFlag.ForEach(Logger.LogInfo);
+                _saveDataFishFlag.ForEach(Logger.LogError);
+                _saveDataMiscFlag.ForEach(Logger.LogWarning);
+                _saveDataLetterFlag.ForEach(Logger.LogMessage);
+                _saveDataGeneralFlag.ForEach(Logger.LogInfo);
+                Logger.LogFatal("Current Level: " + scrTrainManager.instance.currentLevel);
+            }
+            GUI.color = Color.red;
+            if (GUI.Button(new Rect(290, 170, 100, 20), "RESET"))
+            {
+                //scrGameSaveManager.instance.loaded=false;
+                //scrGameSaveManager.instance.LoadGame();
+                //scrWorldSaveDataContainer.instance.LoadWorld();
+                //print(scrWorldSaveDataContainer.instance.coinFlags);
+                //Logger.LogMessage(scrWorldSaveDataContainer.instance.coinFlags);
+                //Logger.LogMessage(scrGameSaveManager.instance.copiedGameData.generalGameData.generalFlags);
+                scrWorldSaveDataContainer.instance.gameSaveManager.ClearSaveData();
+                scrGameSaveManager.instance.LoadGame();
+            }
+            GUI.color = Color.white;
+            if (GUI.Button(new Rect(120, 50, 100, 20), "Add Flag"))
+            {
+                var temp = _currentLevel;
+                var pls = scrGameSaveManager.instance.gameData.worldsData; // Enthält alle welten/level
+                for (int i = 0; i < pls.Count; i++)
+                {
+                    if (i == 0)
+                    {
+                        pls[i].letterFlags.Add("letter12");
+                    }
+                    Logger.LogFatal($"This is Level '{i}'!");
+                    pls[i].coinFlags.ForEach(Logger.LogInfo); //Gibt von welt 0-6 coinFlags | 7-15 existieren nicht bzw. cutscenes
+                }
+                _unlockedLevels[7] = true; // Schaltet ein Level frei hier (7) = Tadpole HQ
+                // scrGameSaveManager.instance.gameData.generalGameData.currentLevel = 1;
+                // scrTrainManager.instance.UseTrain(temp+1, false);
+            }
+            if (GUI.Button(new Rect(120, 170, 100, 20), "Notification"))
+            {
+                NewItem();
+            }
+
+            if (GUI.Button(new Rect(120, 20, 100, 20), "Alle Flags"))
+            {
+                var pls = scrGameSaveManager.instance.gameData.worldsData; // Enthält alle welten/level
+                for (int i = 0; i < pls.Count; i++)
+                {
+                    Logger.LogFatal($"This is Level '{i}'!");
+                    pls[i].coinFlags.ForEach(Logger.LogInfo); //Gibt von welt 0-6 coinFlags | 7-15 existieren nicht bzw. cutscenes
+                    Logger.LogFatal("Cassette Flags:");
+                    pls[i].cassetteFlags.ForEach(Logger.LogInfo);
+                    Logger.LogFatal("Letter Flags:");
+                    pls[i].letterFlags.ForEach(Logger.LogInfo);
+                    Logger.LogFatal("Misc Flags:");
+                    pls[i].miscFlags.ForEach(Logger.LogInfo);
+                }
+                Logger.LogFatal("Current Level: " + scrTrainManager.instance.currentLevel);
+            }
+            GUI.DragWindow(new Rect(0, 0, 10000, 10000));
+        }
+        
+        public void OnGUI()
+        {
+            windowRect = GUI.Window(0, windowRect, WindoFunc, "DEBUG");
+            
+            float test = 0.0f;
+            test = GUI.HorizontalSlider(new Rect(20, 20, 100, 20), test, 0.0f, 15.0f);
+            if (GUI.Button(new Rect(20, 50, 100, 20), "Load Area"))
+            {
+                scrTrainManager.instance.UseTrain((int)test, false);
+            }
+            /*
+             * #Not sure how to make it look nice, preferably use the in-game ui
+             *  to create a server button to join an archipelago session + create a new savefile for it
+             *
+             * #Find a way to use the in-game Notification system to show item received and send
+             *  with the AP logo preferably and no noise/voice
+             */
+            // GUI.Label(new Rect(10, 10, 200, 20), "Hello, IMGUI!");
+            // if (GUI.Button(new Rect(10, 40, 100, 20), "+1 Coin"))
+            // {
+            //     scrGameSaveManager.instance.gameData.generalGameData.coinAmount++;
+            // }
+            
+            // GUI.Box(new Rect(10, 10, 100, 110), "Plugin Menu");
+            // if (GUI.Button(new Rect(20, 40, 80, 20), "Add 1 Coin"))
+            // {
+            //     scrGameSaveManager.instance.gameData.generalGameData.coinAmount++;
+            // }
+        }
+
+    }
+
+    public class ImGUI : MonoBehaviour
+    {
+        public string text = string.Empty;
+    }
+
+    [HarmonyPatch]
+    [HarmonyPatch(typeof(MainMenu), nameof(MainMenu.Instance))]
+    class ApMenu
+    { 
+        public CustomButton apTestButton;
+        [HarmonyPostfix]
+        public void Menu(MainMenu __instance)
+        {
+            __instance.OptionsButton.DefaultColor.g=4F;
+            var apButton = this.apTestButton;
+            if (apButton != null)
+            {
+                apButton.OnClickFinished.AddListener(new UnityAction(MainMenu.Instance.OnExitButtonPressed));
+            }
+        }
+        
+    }
+}
