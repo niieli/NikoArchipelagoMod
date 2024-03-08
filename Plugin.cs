c46a5d3e467f363e3aa97c9d44986cc64ae6370e
niieli <96092663+niieli@users.noreply.github.com>
2024-03-08 16:29:58 +0100

    Ty alwaysintreble for the BepinEx 5 template!

--- Plugin.cs
+++ Plugin.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using BepInEx;
+using BepInEx.Logging;
 using HarmonyLib;
 using HarmonyLib.Tools;
 using KinematicCharacterController.Core;
+using NikoArchipelago.Archipelago;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.PlayerLoop;
@@ -28,7 +30,12 @@ namespace NikoArchipelago
         
         private const string PLUGIN_GUID = "NikoArchipelago";
         private const string PLUGIN_NAME = "NikoArchipelago";
-        private const string PLUGIN_VERSION = "0.0.0";
+        private const string PLUGIN_VERSION = "0.0.1";
+        
+        public const string ModDisplayInfo = $"{PLUGIN_NAME} v{PLUGIN_VERSION}";
+        private const string APDisplayInfo = $"Archipelago v{ArchipelagoClient.APVersion}";
+        public static ManualLogSource BepinLogger;
+        public static ArchipelagoClient ArchipelagoClient;
         
         public Notification test;
         public static CustomButton AptestButton;
@@ -40,6 +47,9 @@ namespace NikoArchipelago
         
         private void Awake()
         {
+            BepinLogger = Logger;
+            ArchipelagoClient = new ArchipelagoClient();
+            ArchipelagoConsole.Awake();
             // Plugin startup logic
             Logger.LogInfo($"Hey, Niko here! btw Plugin {PLUGIN_NAME} Loaded! :)");
         }
@@ -283,20 +293,48 @@ namespace NikoArchipelago
             GUI.DragWindow(new Rect(0, 0, 10000, 10000));
         }
         
-        public void OnGUI()
+        private void OnGUI()
         {
-            windowRect = GUI.Window(0, windowRect, WindowFunc, "DEBUG");
-            // GUI.Label(new Rect(10, 10, 200, 20), "Hello, IMGUI!");
-            // if (GUI.Button(new Rect(10, 40, 100, 20), "+1 Coin"))
-            // {
-            //     scrGameSaveManager.instance.gameData.generalGameData.coinAmount++;
-            // }
-            
-            // GUI.Box(new Rect(10, 10, 100, 110), "Plugin Menu");
-            // if (GUI.Button(new Rect(20, 40, 80, 20), "Add 1 Coin"))
-            // {
-            //     scrGameSaveManager.instance.gameData.generalGameData.coinAmount++;
-            // }
+            // show the mod is currently loaded in the corner
+            GUI.Label(new Rect(16, 16, 300, 20), ModDisplayInfo);
+            ArchipelagoConsole.OnGUI();
+
+            string statusMessage;
+            // show the Archipelago Version and whether we're connected or not
+            if (ArchipelagoClient.Authenticated)
+            {
+                // if your game doesn't usually show the cursor this line may be necessary
+                // Cursor.visible = false;
+
+                statusMessage = " Status: Connected";
+                GUI.Label(new Rect(16, 50, 300, 20), APDisplayInfo + statusMessage);
+            }
+            else
+            {
+                // if your game doesn't usually show the cursor this line may be necessary
+                // Cursor.visible = true;
+
+                statusMessage = " Status: Disconnected";
+                GUI.Label(new Rect(16, 50, 300, 20), APDisplayInfo + statusMessage);
+                GUI.Label(new Rect(16, 70, 150, 20), "Host: ");
+                GUI.Label(new Rect(16, 90, 150, 20), "Player Name: ");
+                GUI.Label(new Rect(16, 110, 150, 20), "Password: ");
+
+                ArchipelagoClient.ServerData.Uri = GUI.TextField(new Rect(150, 70, 150, 20),
+                    ArchipelagoClient.ServerData.Uri);
+                ArchipelagoClient.ServerData.SlotName = GUI.TextField(new Rect(150, 90, 150, 20),
+                    ArchipelagoClient.ServerData.SlotName);
+                ArchipelagoClient.ServerData.Password = GUI.TextField(new Rect(150, 110, 150, 20),
+                    ArchipelagoClient.ServerData.Password);
+
+                // requires that the player at least puts *something* in the slot name
+                if (GUI.Button(new Rect(16, 130, 100, 20), "Connect") &&
+                    !ArchipelagoClient.ServerData.SlotName.IsNullOrWhiteSpace())
+                {
+                    ArchipelagoClient.Connect();
+                }
+            }
+            // this is a good place to create and add a bunch of debug buttons
         }
 
     }
