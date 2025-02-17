f20c30c1feb147f90a589663b40fc5a1d6bb0992
niieli <96092663+niieli@users.noreply.github.com>
2025-02-18 00:55:46 +0100

    Niko now checks if the colliding object is "Kickable" and only then applies the fix

--- Patches/NikoNoMoreFlying.cs
+++ Patches/NikoNoMoreFlying.cs
@@ -1,26 +1,39 @@
-﻿using System;
-using HarmonyLib;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection.Emit;
-using KinematicCharacterController;
+﻿using HarmonyLib;
 using KinematicCharacterController.Core;
 using UnityEngine;
 
-namespace NikoArchipelago.Patches;
-
-public class NikoNoMoreFlying
+namespace NikoArchipelago.Patches
 {
     [HarmonyPatch(typeof(MyCharacterController), "UpdateVelocity")]
     public class MyCharacterControllerUpdateVelocityPatch
     {
-        [HarmonyPrefix]
-        static bool Postfix(MyCharacterController __instance)
+        static void Postfix(ref Vector3 currentVelocity, float deltaTime, MyCharacterController __instance)
         {
-            __instance.Motor.SetPositionAndRotation(new Vector3(__instance.Motor.InterpolatedPosition.x, Mathf.Max(__instance.Motor.InterpolatedPosition.y, __instance.levelBottom.position.y), __instance.Motor.InterpolatedPosition.z), __instance.Motor.GetState().Rotation, false);
-            if ((double) __instance.Motor.InterpolatedPosition.y <= (double) __instance.levelBottom.position.y)
-                return false;
-            return true;
+            // Prüfen, ob "Kickable" in der Nähe ist
+            if (IsTouchingKickable(__instance))
+            {
+                //Plugin.BepinLogger.LogInfo("Kickable GameObject berührt!");
+                __instance.Motor.SetPositionAndRotation(
+                    new Vector3(
+                        __instance.Motor.InterpolatedPosition.x, 
+                        Mathf.Max(__instance.Motor.InterpolatedPosition.y, __instance.levelBottom.position.y),
+                        __instance.Motor.InterpolatedPosition.z),
+                    __instance.Motor.GetState().Rotation, false);
+            }
+        }
+
+        // Methode, um zu prüfen, ob sich "Kickable"-Objekte in Berührung befinden
+        private static bool IsTouchingKickable(MyCharacterController characterController)
+        {
+            Collider[] hitColliders = Physics.OverlapSphere(characterController.transform.position, 1.0f); // Radius = 1.0f
+            foreach (var hit in hitColliders)
+            {
+                if (hit.CompareTag("Kickable"))
+                {
+                    return true;
+                }
+            }
+            return false;
         }
     }
 }
