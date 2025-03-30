18495ddadef21f66a1442bc98f64d6aaeb37a0e6
niieli <96092663+niieli@users.noreply.github.com>
2025-03-30 20:07:42 +0200

    Make Letters not popup when picking one up

---
+++ Patches/LetterSkipPatch.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using KinematicCharacterController.Core;
+
+namespace NikoArchipelago.Patches;
+
+public class LetterSkipPatch
+{
+    [HarmonyPatch(typeof(scrEnvelope), "Update")]
+    public static class LetterDontOpen
+    {
+        private static void Postfix(scrEnvelope __instance)
+        {
+            if (__instance.trigger.foundPlayer())
+            {
+                scrLetterDisplayer.instance.showLetters = false;
+                MyCharacterController.instance.blockMovementInput = false;
+            }
+        }
+    }
+}
